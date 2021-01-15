62308aa8c949fe7104ffa4482a91301c4c045df7
David <oliveiradavid.dev@gmail.com>
2021-01-15 03:14:37 -0400

    Add form to register a new project

---
+++ src/rules/index.js
@@ -0,0 +1,18 @@
+const required = v => !!v || 'O campo é obrigatório.';
+
+const rules = {
+  name: { required },
+  end_date: { required },
+  start_date: { required },
+  participants: {
+    atLeastOne: array =>
+      (!!array && array.length > 0) ||
+      'Necessário informar pelo menos 1 participante.'
+  },
+  risk: {
+    required: value =>
+      [0, 1, 2].indexOf(value) !== -1 || 'O campo é obrigatório.'
+  }
+};
+
+export default rules;
--- src/views/CreateProject.vue
+++ src/views/CreateProject.vue
@@ -6,7 +6,7 @@
     <PageContent>
       <v-container>
         <v-form>
-          <v-row>
+          <v-row align-sm="center" justify-sm="center">
             <v-col cols="12" sm="8">
               <v-text-field
                 v-model="project.name"
@@ -18,12 +18,13 @@
             </v-col>
           </v-row>
 
-          <v-row wrap>
+          <v-row wrap align-sm="center" justify-sm="center">
             <v-col cols="12" sm="4">
               <v-text-field
                 v-model="project.start_date"
                 label="Data de início"
                 hint="DD/MM/YYYY"
+                :rules="[(v) => !!v]"
                 prepend-icon="mdi-calendar-start"
                 required
               ></v-text-field>
@@ -33,13 +34,14 @@
                 v-model="project.end_date"
                 label="Data de término"
                 hint="DD/MM/YYYY"
+                :rules="[(v) => !!v]"
                 prepend-icon="mdi-calendar-end"
                 required
               ></v-text-field>
             </v-col>
           </v-row>
 
-          <v-row>
+          <v-row align-sm="center" justify-sm="center">
             <v-col cols="12" sm="4">
               <v-text-field
                 v-model="project.value"
@@ -53,10 +55,11 @@
             <v-col cols="12" sm="4">
               <v-autocomplete
                 v-model="project.risk"
-                :rules="[() => project.risk >= 0 || 'O campo é obrigatório']"
+                :rules="[() => project.risk || 'O campo é obrigatório']"
                 :items="risks"
                 item-value="level"
                 item-text="label"
+                prepend-icon="mdi-alert"
                 label="Risco"
                 placeholder="Selecione um nível"
                 required
@@ -64,33 +67,47 @@
             </v-col>
           </v-row>
 
-          <v-row>
-            <v-col cols="12" sm="4">
+          <v-row class="mt-4" align-sm="center" justify-sm="center">
+            <v-col cols="12" sm="8">
+              <v-label> Participantes </v-label>
+              <div>
+                <v-chip
+                  v-for="person in project.participants"
+                  :key="person.name"
+                  class="ma-2"
+                  close
+                  @click:close="removeParticipant(person.name)"
+                >
+                  <v-icon left> mdi-account </v-icon>
+                  <span> {{ person.name }} </span>
+                </v-chip>
+              </div>
+
               <v-text-field
                 v-model="participant"
                 label="Adicionar participante"
+                prepend-icon="mdi-account-circle"
                 append-outer-icon="mdi-plus"
+                required
+                :rules="[
+                  () =>
+                    project.participants.length > 0 || 'O campo é obrigatório.'
+                ]"
+                @click:append-outer="addParticipant(participant)"
+                @keyup.enter="addParticipant(participant)"
               ></v-text-field>
-
-              <v-label> Participantes </v-label>
-              <div v-for="person in project.participants" :key="person.name">
-                <v-icon left> mdi-account-circle </v-icon>
-
-                <span>
-                  {{ person.name }}
-                </span>
-              </div>
             </v-col>
           </v-row>
 
           <v-row>
-            <v-col cols="12">
-              <v-btn> Criar projeto </v-btn>
+            <v-col cols="12" class="mt-4 d-flex justify-center align-center">
+              <v-btn color="secondary" depressed small>
+                Salvar novo projeto
+              </v-btn>
             </v-col>
           </v-row>
         </v-form>
       </v-container>
-      <v-form> </v-form>
     </PageContent>
   </PageWrapper>
 </template>
@@ -114,7 +131,8 @@ export default {
         end_date: null,
         value: null,
         risk: null,
-        participants: [{ name: 'Foo' }]
+        participants: []
+        // [{ name: 'Foo' }, { name: 'Foo2' }]
       },
       participant: null,
       risks: [
@@ -123,6 +141,25 @@ export default {
         { level: 2, label: 'Alto' }
       ]
     };
+  },
+  methods: {
+    addParticipant(participant) {
+      if (participant && this.participantDoesNotExists(participant)) {
+        this.project.participants.push({ name: participant });
+        this.clearInputParticipant();
+      }
+    },
+    clearInputParticipant() {
+      this.participant = null;
+    },
+    removeParticipant(name) {
+      this.project.participants = this.project.participants.filter(
+        (p) => p.name !== name
+      );
+    },
+    participantDoesNotExists(participant) {
+      return !this.project.participants.find((p) => p.name === participant);
+    }
   }
 };
 </script>
