0d0d8ba6c27e985974ac2ddda92a869d361a3ebd
David <oliveiradavid.dev@gmail.com>
2021-01-19 04:40:40 -0400

    Add confirmatino dialog to delete button

--- src/components/buttons/ButtonDelete.vue
+++ src/components/buttons/ButtonDelete.vue
@@ -1,7 +1,21 @@
 <template>
-  <v-btn x-small fab color="error" @click="onClick">
-    <v-icon>mdi-delete</v-icon>
-  </v-btn>
+  <v-dialog v-model="dialog" persistent max-width="290">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn x-small fab color="error" v-bind="attrs" v-on="on">
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title class="text-h6"
+        >Deseja realmente excluir o projeto?
+      </v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text @click="dialog = false"> Cancelar </v-btn>
+        <v-btn color="primary" text @click="handleDelete"> Excluir </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -11,6 +25,17 @@ export default {
     onClick: {
       required: true,
       type: Function
+    }
+  },
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  methods: {
+    handleDelete() {
+      this.onClick();
+      this.dialog = false;
     }
   }
 };
